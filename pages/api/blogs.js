35f58994312151b63498a6765e758af9e0f6a1a2
Pooja Paul <71924871+Pooja113@users.noreply.github.com>
2022-05-08 18:22:13 +0530

    Clean Up

--- pages/api/blogs.js
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-
-export default function handler(req,res){
-  fs.readFile('blogdata/user1.json','utf-8',(err,data)=>{
-    res.status(200).json(JSON.parse(data))
-  })
-  
-}
